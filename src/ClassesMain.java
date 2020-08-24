06faab02b90804a657b66ede50ba4a1dc7a253b3
alwinwork <alwin.solanky@sogeti.com>
2020-08-24 11:44:06 +0100

    cars times three

--- src/ClassesMain.java
+++ src/ClassesMain.java
@@ -1,5 +1,28 @@
 public class ClassesMain {
     public static void main(String[] args){
 
+        Car ferrari = new Car();
+
+        ferrari.model = "Ferrari F430";
+        ferrari.seats = 2;
+        ferrari.colour = "Green";
+
+        ferrari.display();
+
+        Car audi = new Car();
+
+        audi.model = "A3 Qauttro";
+        audi.seats = 4;
+        audi.colour = "Black";
+
+        audi.display();
+
+        Car toyota = new Car();
+
+        toyota.model = "Hillux";
+        toyota.seats = 3;
+        toyota.colour = "Red";
+
+        toyota.display();
     }
 }
